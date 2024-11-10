f7ba9d2de664927819e6ec43adfad67b61433b5b
zushii <zxgamingh@gmail.com>
2024-11-10 17:44:40 +0100

    new popup

--- src/components/CarContainer.js
+++ src/components/CarContainer.js
@@ -1,17 +1,17 @@
 const displayed_cars = [
-  {title:"Galaxy Destroyer", info:"short description of car", image:"placeholder.jpg", price:500, id:1},
-  {title:"Star Devourer", info:"short description of car", image:"placeholder2.jpg", price:500, id:2},
-  {title:"World Ender", info:"short description of car", image:"placeholder3.jpg", price:500, id:3},
-  {title:"Asteroid Demolisher", info:"short description of car", image:"placeholder4.jpg", price:500, id:4},
-  {title:"Glorp", info:"Glorp", image:"glorp.jpg", price:500, id:5}
+  {title:"GalaxyDestroyer", model:"F20", year:"2006", value:500, info:"short description of car", image:"placeholder.jpg", id:1},
+  {title:"StarDevourer", model:"F20", year:"2006", value:500, info:"short description of car", image:"placeholder2.jpg", id:2},
+  {title:"WorldEnder", model:"F20", year:"2006", value:500, info:"short description of car", image:"placeholder3.jpg", id:3},
+  {title:"AsteroidDemolisher", model:"F20", year:"2006", value:500, info:"short description of car", image:"placeholder4.jpg", id:4},
+  {title:"Glorp", model:"F20", year:"2006", value:500, info:"Glorp", image:"glorp.jpg", id:5}
 ]
 
 const car_details = [
-  {id:1, extInfo:"more info", engine:"engine", horsePower:"1000"},
-  {id:2, extInfo:"more info", engine:"engine", horsePower:"500"},
-  {id:3, extInfo:"more info", engine:"engine", horsePower:"500"},
-  {id:4, extInfo:"more info", engine:"engine", horsePower:"500"},
-  {id:5, extInfo:"more info", engine:"engine", horsePower:"0"}
+  {engine:"2.0L I4 Engine - 150hp", trans:"Manual", milage:"25,000 km", fuel:"Milk", id:1},
+  {engine:"2.0L I4 Engine - 150hp", trans:"Manual", milage:"25,000 km", fuel:"Milk", id:2},
+  {engine:"2.0L I4 Engine - 150hp", trans:"Manual", milage:"25,000 km", fuel:"Milk", id:3},
+  {engine:"2.0L I4 Engine - 150hp", trans:"Manual", milage:"25,000 km", fuel:"Milk", id:4},
+  {engine:"2.0L I4 Engine - 150hp", trans:"Manual", milage:"25,000 km", fuel:"Milk", id:5}
 ]
 
 export default function CarContainer(){
@@ -34,12 +34,12 @@ export default function CarContainer(){
         <img src={item.image} alt="this is car" className="car-image"></img>
 
         <section className="text-contents">
-          <h3 className="car-name">{item.title}</h3>
-          <span>{item.price} Dabloons</span>
+          <h3 className="car-name">{item.year} {item.title} {item.model}</h3>
+          <span>{item.value} Dabloons</span>
           <p className="car-info">{item.info}</p>
-          <div className="more-button">
+          <div className="details-button">
             <button onClick={() => Popup(displayed_cars, car_details, item.id)}>
-              More
+              Details
             </button>
           </div>
         </section>
@@ -63,8 +63,11 @@ function Popup(list, list2, id){
   const detailsList2 = list2[id-1];
 
   const popup = document.createElement("div");
-  popup.id= "popup";
+  popup.id = "popup";
   popup.classList.add("popup-cover");
+  popup.onclick = function () {
+    removePopup();
+  };
 
   const popupBox = document.createElement("div");
   popupBox.classList.add("popup-style");
@@ -73,32 +76,41 @@ function Popup(list, list2, id){
   img.src = detailsList.image;
   popupBox.appendChild(img);
 
-  const left = document.createElement("section");
-  popupBox.appendChild(left);
-  const right = document.createElement("section");
-  popupBox.appendChild(right);
+  // const left = document.createElement("section");
+  // popupBox.appendChild(left);
+  // const right = document.createElement("section");
+  // popupBox.appendChild(right);
+
   const buttonContainer = document.createElement("div");
   buttonContainer.classList.add("button-container");
 
   const name = document.createElement("h4");
-  name.innerText = detailsList.title;
-  left.appendChild(name);
+  name.innerText = `${detailsList.year} ${detailsList.title} ${detailsList.model}`;
+  popupBox.appendChild(name);
+
+  const leftList = document.createElement("ul");
+  popupBox.appendChild(leftList);
+  const rightList = document.createElement("ul");
+  popupBox.appendChild(rightList);
+
+  const listKeys = Object.keys(detailsList);
+  for (let i = 0; i < 4; i++){
+    const upperCaseKey = listKeys[i].charAt(0).toUpperCase() + listKeys[i].slice(1);
+    const e = document.createElement("li");
+    e.innerHTML = `${upperCaseKey}: ${detailsList[listKeys[i]]}`;
+    leftList.appendChild(e);
+  }
+  const infoText = document.createElement("li");
+  infoText.innerHTML = detailsList.info;
+  leftList.appendChild(infoText);
   
-  const info = document.createElement("p");
-  info.innerText = detailsList2.extInfo;
-  left.appendChild(info);
-
-  const eng = document.createElement("span");
-  eng.innerText = detailsList2.engine;
-  right.appendChild(eng);
-
-  const hp = document.createElement("span");
-  hp.innerText = detailsList2.horsePower;
-  right.appendChild(hp);
-
-  const price = document.createElement("span");
-  price.innerText = `${detailsList.price} Dabloons`;
-  right.appendChild(price);
+  const listKeys2 = Object.keys(detailsList2);
+  for (let i = 0; i < 4; i++){
+    const upperCaseKey = listKeys2[i].charAt(0).toUpperCase() + listKeys2[i].slice(1);
+    const e = document.createElement("li");
+    e.innerHTML = `${upperCaseKey}: ${detailsList2[listKeys2[i]]}`;
+    rightList.appendChild(e);
+  }
 
   const backButton = document.createElement("button");
   backButton.innerText = "Back"
